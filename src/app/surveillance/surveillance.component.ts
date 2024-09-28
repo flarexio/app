3809ec3bce0ed6025b15239163eb07dbed6c9543
Lin, Ying-Chin <mirror770109@gmail.com>
2024-09-28 17:35:51 +0800

    added video track

--- src/app/surveillance/surveillance.component.ts
+++ src/app/surveillance/surveillance.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { concatMap, filter, from, interval, map, take, switchMap } from 'rxjs';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { filter, from, switchMap } from 'rxjs';
 
 import { Empty, MsgHdrsImpl, NatsConnection, createInbox } from '@nats-io/nats-core';
 
@@ -13,17 +13,15 @@ import { NatsService } from '../nats.service';
   styleUrl: './surveillance.component.scss'
 })
 export class SurveillanceComponent {
+  @ViewChild('remoteVideo')
+  remoteVideo!: ElementRef<HTMLVideoElement>;
+
   constructor(
     private natsService: NatsService,
   ) {
     this.natsService.connectionChange.pipe(
       filter((nc): nc is NatsConnection => nc != undefined),
       switchMap((nc) => from(this.makePeer(nc))),
-      concatMap((channel) => interval(10).pipe(
-        take(1000),
-        filter(() => channel.readyState == 'open'),
-        map((n) => channel.send(n.toString())),
-      ))
     ).subscribe({
       error: (err) => console.error(err),
       complete: () => console.log('complete'),
@@ -54,20 +52,53 @@ export class SurveillanceComponent {
       }
     });
 
-    peer.onicecandidate = (event) => {
+    peer.addEventListener('connectionstatechange', (_) => {
+      console.log(`connection state changed: ${peer.connectionState}`);
+    });
+
+    peer.addEventListener('icecandidate', (event) => {
       const candidate = event.candidate;
       if (candidate == null) return;
 
       const payload = JSON.stringify(candidate);
       nc.publish(`${inbox}.candidates.caller`, payload);
-    };
+    });
 
-    const channel = peer.createDataChannel('sendDataChannel');
-    channel.onopen = (_) => console.log('channel opened');
-    channel.onclose = (_) => console.log('channel closed');
+    peer.addEventListener('track', async (event) => {
+      console.log(event);
+
+      const stream = event.streams[0];
+      this.remoteVideo.nativeElement.srcObject = stream;
+    });
+
+    peer.addTransceiver('video', { direction: 'recvonly' });
+
+    // const channel = peer.createDataChannel('stream');
+    // channel.addEventListener('open', async (_) => {
+    //   console.log('channel opened');
+
+    //   // const msg = await nc.request("stream.origins", Empty, { timeout: 5000, noMux: true });
+    //   // const origins = JSON.parse(msg.string()) as number[];
+
+    //   // const buf = new ArrayBuffer(4);
+    //   // const view = new DataView(buf);
+    //   // view.setUint32(0, origins[0]);
+
+    //   // channel.send(buf);
+
+    //   channel.send('play');
+    // });
+
+    // channel.addEventListener('close', (_) => console.log('channel closed'));
 
     try {
-      const offer = await peer.createOffer();
+      const offerOpts: RTCOfferOptions = {
+        iceRestart: true,
+        offerToReceiveVideo: true,
+        offerToReceiveAudio: false,
+      };
+
+      const offer = await peer.createOffer(offerOpts);
       console.log('Offer from peerConnection');
       console.log(offer.sdp);
 
@@ -87,7 +118,5 @@ export class SurveillanceComponent {
     } catch (err) {
       console.error(err);
     }
-
-    return channel;
   }
 }
